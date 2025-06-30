855835163a6fc63d30e7aa8c61ea24e8855939ab
integral-training <integral.training.2025@gmail.com>
2025-06-30 14:49:10 +0900

    修正済

--- C++kadai/kadai05.cpp
+++ C++kadai/kadai05.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <limits> // cin初期化に関係
+
 enum TELEPHONE_ACTION : int // 電話の動作
 {
     INCOM   = 1, // 着信
@@ -8,6 +9,7 @@ enum TELEPHONE_ACTION : int // 電話の動作
     END     = 4, // 通話終了
     OFF     = 5  // 電源OFF
 };
+
 enum TELEPHONE_STATUS : int // 電話の状態
 {
     IDLE        = 1, // 待機中
@@ -15,13 +17,15 @@ enum TELEPHONE_STATUS : int // 電話の状態
     INCOMING    = 3, // 着信中
     ONCALLING   = 4, // 通話中
 };
+
+
 // 定型文関数。引数に電話の状態を受け取る。
 int inputTelephoneAction(int status)
 {
-    while(true)
+    while (true)
     {
         std::string message = "[状態] ";
-        if(status == TELEPHONE_STATUS::IDLE)
+        if (status == TELEPHONE_STATUS::IDLE)
         {
             message += "待機中";
         }
@@ -37,9 +41,16 @@ int inputTelephoneAction(int status)
         {
             message += "通話中";
         }
+        else
+        {
+            message += "不明";
+        }
+
         std::cout << message << std::endl;
         std::cout << std::endl;
+
         int inputAction = 0; // 電話の動作を決める入力値
+
         std::cout << "操作を選んでください:" << std::endl;
         std::cout << "1. 着信" << std::endl;
         std::cout << "2. 発信" << std::endl;
@@ -48,30 +59,31 @@ int inputTelephoneAction(int status)
         std::cout << "5. 電源OFF" << std::endl;
         std::cout << "番号を入力: ";
         std::cin >> inputAction;
-        if(std::cin.fail())
-        {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cout << "無効な入力です。1～5の数字を入力してください。" << std::endl;
-        }
-        else if(inputAction < 1 || inputAction > 5)
+
+        if (std::cin.fail())
         {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             std::cout << "無効な入力です。数値で選択してください。" << std::endl;
         }
+        else if (inputAction < 1 || inputAction > 5)
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "無効な入力です。1～5の数字を入力してください。" << std::endl;
+        }
         else
         {
             return inputAction;
-            break;
         }
     }
 }
-// 1 = 着信動作。電話の状態を受け取り、遷移可能な状態かによって処理が分岐
+
+// 着信動作。電話の状態を受け取り、遷移可能な状態かによって処理が分岐
 void actionIncoming(int& currentTelStatus)
 {
     // 待機状態なら着信に遷移可
-    if(currentTelStatus == TELEPHONE_STATUS::IDLE)
+    if (currentTelStatus == TELEPHONE_STATUS::IDLE)
     {
         currentTelStatus = TELEPHONE_STATUS::INCOMING;
         // 電話の状態を着信に変更
@@ -81,79 +93,90 @@ void actionIncoming(int& currentTelStatus)
         std::cout << "着信できるのは待機中のみです。" << std::endl;
     }
 }
-// 2 = 発信動作。電話の状態を受け取り、遷移可能な状態かによって処理が分岐
+
+// 発信動作。電話の状態を受け取り、遷移可能な状態かによって処理が分岐
 void actionCalling(int& currentTelStatus)
 {
     // 待機状態なら発信に遷移可
-    if(currentTelStatus == TELEPHONE_STATUS::IDLE)
+    if (currentTelStatus == TELEPHONE_STATUS::IDLE)
     {
-        currentTelStatus = TELEPHONE_STATUS::CALLING;
         // 電話の状態を発信に変更
+        currentTelStatus = TELEPHONE_STATUS::CALLING;
     }
     else
     {
         std::cout << "発信できるのは待機中のみです。" << std::endl;
     }
 }
-// 3 = 受話動作。電話の状態を受け取り、遷移可能な状態かによって処理が分岐
+
+// 受話動作。電話の状態を受け取り、遷移可能な状態かによって処理が分岐
 void actionReceiving(int& currentTelStatus)
 {
     // 着信状態 or 発信状態なら通話に遷移可
-    if(currentTelStatus == TELEPHONE_STATUS::CALLING || currentTelStatus == TELEPHONE_STATUS::INCOMING)
+    if (currentTelStatus == TELEPHONE_STATUS::CALLING || currentTelStatus == TELEPHONE_STATUS::INCOMING)
     {
-        currentTelStatus = TELEPHONE_STATUS::ONCALLING;
         // 電話の状態を通話に変更
+        currentTelStatus = TELEPHONE_STATUS::ONCALLING;
     }
-    else // if(currentTelStatus == TELEPHONE_STATUS::IDLE || currentTelStatus == TELEPHONE_STATUS::ONCALLING)
+    else
     {
         std::cout << "受話できるのは着信中または発信中のみです。" << std::endl;
     }
 }
-//通話終了動作。電話の状態を受け取り、遷移可能な状態かによって処理が分岐
+
+// 通話終了動作。電話の状態を受け取り、遷移可能な状態かによって処理が分岐
 void actionEnd(int& currentTelStatus)
 {
     // 通話状態なら待機に遷移可
-    if(currentTelStatus != TELEPHONE_STATUS::IDLE)
+    if (currentTelStatus != TELEPHONE_STATUS::IDLE)
     {
-        currentTelStatus = TELEPHONE_STATUS::IDLE;
         // 電話の状態を待機に変更
+        currentTelStatus = TELEPHONE_STATUS::IDLE;
     }
     else
     {
         std::cout << "現在通話中ではありません。" << std::endl;
     }
 }
+
+
 int main()
 {
-    int Action = 0; // 関数から受け取る行動値
+    int action = 0; // 関数から受け取る行動値
     bool loop = true; // do-while条件
     int currentTelStatus = TELEPHONE_STATUS::IDLE; // 初期状態は待機中
     std::cout << "携帯電話ステートマシン" << std::endl;
-    while(loop)
+
+    while (loop)
     {
-        Action = inputTelephoneAction(currentTelStatus); // 電話の状態を定型文関数に渡す
+        action = inputTelephoneAction(currentTelStatus); // 電話の状態を定型文関数に渡す
         // currentTelStatus状態のとき、電話のACTIONを起こせるか判別する
-        if(Action == TELEPHONE_ACTION::INCOM) // 1 = 着信
+        if (action == TELEPHONE_ACTION::INCOM) // 1 = 着信
         {
             actionIncoming(currentTelStatus);
         }
-        else if(Action == TELEPHONE_ACTION::CALL) // 2 = 発信
+        else if (action == TELEPHONE_ACTION::CALL) // 2 = 発信
         {
             actionCalling(currentTelStatus);
         }
-        else if(Action == TELEPHONE_ACTION::RECEIVE) // 3 = 受話
+        else if (action == TELEPHONE_ACTION::RECEIVE) // 3 = 受話
         {
             actionReceiving(currentTelStatus);
         }
-        else if(Action == TELEPHONE_ACTION::END) // 4 = 通話終了
+        else if (action == TELEPHONE_ACTION::END) // 4 = 通話終了
         {
             actionEnd(currentTelStatus);
         }
-        else if(Action == TELEPHONE_ACTION::OFF) // 5 = 電源OFF
+        else if (action == TELEPHONE_ACTION::OFF) // 5 = 電源OFF
         {
             std::cout << "携帯電話の電源を切りました。" << std::endl;
             loop = false;
         }
+        else
+        {
+            std::cout << "不明なアクションです。" << std::endl;
+        }
     }
+
     return 0;
 }
